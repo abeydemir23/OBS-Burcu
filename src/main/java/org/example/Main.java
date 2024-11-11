cf03a2eb6504a595c4af2ccd7fd1cc14ac5446e6
Alican Beydemir <alican.beydemir1@huawei.com>
2024-11-11 15:11:49 +0300

    Refactor revert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.*;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -32,11 +29,12 @@ public class Main {
         Long afterAdQuitMillis = (Long) jsonObject.get("afterAdQuitMillis");
         Long adLoopCount = (Long) jsonObject.get("adLoopCount");
 
-        ScheduledThreadPoolExecutor threadPool;
-        threadPool = new ScheduledThreadPoolExecutor(2);
+        ScheduledThreadPoolExecutor threadPool
+                = new ScheduledThreadPoolExecutor(2);
         // WebDriver'ı başlat
         Runnable adWatcher = new AdWatcher(username, password, loginWaitMillis, adWaitMillis, afterAdQuitMillis, adLoopCount, schedulePeriodMin, threadPool);
-        ScheduledFuture<?> future = threadPool.scheduleAtFixedRate(adWatcher, 0, schedulePeriodMin, TimeUnit.MINUTES);
+
+        threadPool.scheduleAtFixedRate(adWatcher, 0, schedulePeriodMin, TimeUnit.MINUTES);
     }
 
     static class AdWatcher implements Runnable {
@@ -73,7 +71,18 @@ public class Main {
                 drivers.add(driver);
                 watchAd(driver, username, password, loginWaitMillis, adWaitMillis, afterAdQuitMillis, adLoopCount);
             } catch (Exception e) {
-                drivers.stream().map(AdWatcher::closeAndQuitWebDriver);
+                for (WebDriver d : drivers) {
+                    String originalHandle = d.getWindowHandle();
+                    for (String handle : d.getWindowHandles()) {
+                        if (!originalHandle.equals(handle)) {
+                            d.switchTo().window(handle);
+                            d.close();
+                        }
+
+                    }
+                    d.switchTo().window(originalHandle);
+                    d.quit();
+                }
                 drivers.clear();
                 System.err.println(e.getMessage());
                 executor.shutdown();
@@ -82,43 +91,27 @@ public class Main {
             }
         }
 
-        private static WebDriver closeAndQuitWebDriver(WebDriver driver) {
+        private static void watchAd(WebDriver driver, String username, String password, Long loginWaitMillis,
+                                    Long adWaitMillis, Long afterAdQuitMillis, Long adLoopCount) throws InterruptedException {
+            driver.manage().window().maximize();
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            driver.get("https://en.onlinesoccermanager.com/PrivacyNotice?nextUrl=%2FLogin");
+
+
+            login(driver, username, password, loginWaitMillis);
             String originalHandle = driver.getWindowHandle();
-            return driver.getWindowHandles().stream().filter(handle -> !handle.equals(originalHandle)).map(handle -> {
-                System.out.println("Hatalı Pencere Kapanıyor");
-                driver.switchTo().window(handle);
-                driver.close();
-                return driver;
-            }).map(webDriver -> {
-                System.out.println("Hatalı WebDriver Kapanıyor");
-                webDriver.switchTo().window(originalHandle).quit();
-                return webDriver;
-            }).findFirst().get();
-        }
+            WebDriverWait wdw = new WebDriverWait(driver, Duration.ofMillis(loginWaitMillis));
+            Optional<WebElement> agreeButton = driver.findElements(By.cssSelector("body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button")).stream().findFirst();
+            agreeButton.ifPresent(WebElement::click);
 
-        private static void watchAd(WebDriver driver, String username, String password, Long loginWaitMillis, Long adWaitMillis, Long afterAdQuitMillis, Long adLoopCount) throws InterruptedException {
+            WebElement until = wdw.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#balances > div.wallet-container.bosscoin-wallet.btn-new.btn-success > div.wallet-amount.pull-left.center > div > span")));
+            int startCoin = Integer.parseInt(driver.findElement(By.cssSelector("#balances > div.wallet-container.bosscoin-wallet.btn-new.btn-success > div.wallet-amount.pull-left.center > div > span")).getText());
+
             try {
-
-                driver.manage().window().maximize();
-
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                driver.navigate().to("https://en.onlinesoccermanager.com/ChooseLeague");
-                login(driver, username, password, loginWaitMillis);
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
-                driver.get("https://en.onlinesoccermanager.com/BusinessClub");
-//                new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.urlToBe("https://en.onlinesoccermanager.com/BusinessClub"));
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
-                driver.navigate().refresh();
-                String originalHandle = driver.getWindowHandle();
-                int startCoin = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"balances\"]/div/div[1]/div/span")).getText());
-                System.out.println("Başlangıç Coin : " + startCoin);
-                int currentReturnCoin = watchAds(driver, adWaitMillis, adLoopCount);
-                if (currentReturnCoin == 0) {
-                    closeAndQuitWebDriver(driver);
-                    System.out.println("Reklamlar İzlendi Limit Aşıldığı İçin Kapanıyor Mevcut Boss Coin : " + startCoin);
-                    return;
-                }
-                int gained = currentReturnCoin - startCoin;
+                int currentCoin = watchAds(driver, adWaitMillis, adLoopCount);
+                int gained = currentCoin - startCoin;
                 if (gained < 9) {
                     System.out.println("Eksik " + (10 - gained) + "coin tamamlanıyor");
                     for (String handle : driver.getWindowHandles()) {
@@ -131,87 +124,59 @@ public class Main {
                     return;
                 }
 
-
-                System.out.println("Reklamlar İzlendi " + afterAdQuitMillis + " ms Sonra Kapanıyor");
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(afterAdQuitMillis));
-                for (String handle : driver.getWindowHandles()) {
-                    if (!handle.equals(originalHandle)) {
-                        driver.switchTo().window(handle);
-                        driver.close();
-                    }
-                }
-
-                driver.switchTo().window(originalHandle);
-                driver.navigate().refresh();
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(adWaitMillis));
-                WebElement currentCoin = driver.findElement(By.cssSelector("#balances > div > div.wallet-amount.pull-left.center > div > span"));
-                System.out.println("Reklamlar bitti Mevcut Boss Coin : " + currentCoin.getText());
-                driver.quit();
             } catch (Exception e) {
-                if (driver != null) {
-                    String currentHandle = driver.getWindowHandle();
-                    for (String handle : driver.getWindowHandles()) {
-                        if (!handle.equals(currentHandle)) {
-                            driver.switchTo().window(handle);
-                            driver.close();
-                        }
-                    }
-                    driver.switchTo().window(currentHandle).quit();
-
-                }
-                System.out.println("Reklamlar izlenirken hata alındı: " + e.getMessage());
+                System.out.println("Reklam butonuna tıklanamadı: " + e.getMessage());
                 throw e;
             }
+            System.out.println("Reklamlar İzlendi " + afterAdQuitMillis + " ms Sonra Kapanıyor");
+            Thread.currentThread().sleep(afterAdQuitMillis);
+            for (String handle : driver.getWindowHandles()) {
+                if (!handle.equals(originalHandle)) {
+                    driver.switchTo().window(handle);
+                    driver.close();
+                }
+            }
+
+            driver.switchTo().window(originalHandle);
+            driver.navigate().refresh();
+            Thread.currentThread().sleep(adWaitMillis);
+            WebElement currentCoin = driver.findElement(By.cssSelector("#balances > div.wallet-container.bosscoin-wallet.btn-new.btn-success > div.wallet-amount.pull-left.center > div > span"));
+            System.out.println("Reklamlar bitti Mevcut Boss Coin : " + currentCoin.getText());
+            driver.quit();
         }
 
         private static int watchAds(WebDriver driver, Long adWaitMillis, Long adLoopCount) throws InterruptedException {
             for (int i = 0; i < adLoopCount; i++) {
-                if (openAd(driver, adWaitMillis) < 0) {
-                    return 0;
+                Thread.currentThread().sleep(adWaitMillis);
+                Optional<WebElement> agreeButton = driver.findElements(By.cssSelector("body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button")).stream().findFirst();
+                agreeButton.ifPresent(WebElement::click);
+                WebElement currentCoin = driver.findElement(By.cssSelector("#balances > div.wallet-container.bosscoin-wallet.btn-new.btn-success > div.wallet-amount.pull-left.center > div > span"));
+                System.out.println("Mevcut Boss Coin : " + currentCoin.getText());
+                driver.switchTo().newWindow(WindowType.TAB);
+                driver.get("https://en.onlinesoccermanager.com/BusinessClub");
+                Thread.currentThread().sleep(adWaitMillis);
+                WebElement watchAdButton = driver.findElement(By.cssSelector("#body-content > div.row.row-h-sm-600.row-h-md-23.overflow-hidden.theme-stepover-0.businessclub-container > div.col-xs-12.col-h-xs-22.col-h-sm-20.businessclub-rows-container > div > div:nth-child(1) > div"));
+                watchAdButton.click();
+                System.out.println("Reklam başlatıldı.");
+                Thread.currentThread().sleep(adWaitMillis);
+                List<WebElement> elements = driver.findElements(By.cssSelector("#modal-dialog-alert > div.row.row-h-xs-24.overflow-visible.modal-content-container > div > div > div > div.modal-header > h3"));
+                if (elements.size() > 0) {
+                    boolean isLimitReached = elements.get(0).getText().equalsIgnoreCase("Can't show video");
+                    if (isLimitReached) {
+                        System.out.println("Reklam Limiti doldu çıkılıyor");
+                        driver.navigate().refresh();
+                        return Integer.parseInt(driver.findElement(By.cssSelector("#balances > div.wallet-container.bosscoin-wallet.btn-new.btn-success > div.wallet-amount.pull-left.center > div > span")).getText()) + 9;
+                    }
                 }
 
             }
             driver.navigate().to("https://en.onlinesoccermanager.com/BusinessClub");
             driver.navigate().refresh();
-            WebElement currentCoin = driver.findElement(By.cssSelector("#balances > div > div.wallet-amount.pull-left.center > div > span"));
+            WebElement currentCoin = driver.findElement(By.cssSelector("#balances > div.wallet-container.bosscoin-wallet.btn-new.btn-success > div.wallet-amount.pull-left.center > div > span"));
             return Integer.parseInt(currentCoin.getText());
         }
 
-        private static int openAd(WebDriver driver, Long adWaitMillis) throws InterruptedException {
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(adWaitMillis));
-            WebElement currentCoin = driver.findElement(By.cssSelector("#balances > div > div.wallet-amount.pull-left.center > div > span"));
-            System.out.println("Mevcut Boss Coin : " + currentCoin.getText());
-            driver.switchTo().newWindow(WindowType.TAB);
-            driver.get("https://en.onlinesoccermanager.com/BusinessClub");
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(adWaitMillis));
-            WebElement watchAdButton = driver.findElement(By.cssSelector("#body-content > div.row.row-h-sm-600.row-h-md-23.overflow-hidden.theme-stepover-0.businessclub-container > div.col-xs-12.col-h-xs-22.col-h-sm-20.businessclub-rows-container > div > div:nth-child(1) > div"));
-            watchAdButton.click();
-            System.out.println("Reklam başlatılıyor.");
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(adWaitMillis));
-            List<WebElement> adLogo = driver.findElements(By.cssSelector("#aipLogo"));
-            if (!adLogo.isEmpty()) {
-                System.out.println("Reklam Başarılı bir şekilde açıldı");
-            } else {
-
-                List<WebElement> elements = driver.findElements(By.cssSelector("#modal-dialog-alert > div.row.row-h-xs-24.overflow-visible.modal-content-container > div > div > div > div.modal-header > h3"));
-                if (!elements.isEmpty()) {
-                    boolean isLimitReached = elements.get(0).getText().equalsIgnoreCase("Can't show video");
-                    if (isLimitReached) {
-                        System.out.println("Reklam Limiti doldu çıkılıyor");
-                        driver.navigate().refresh();
-                        return -1;
-                    }
-                }
-
-                System.out.println("Reklam açılmadı tekrar deneniyor");
-                driver.close();
-                openAd(driver, adWaitMillis);
-            }
-            return 0;
-        }
-
         private static void login(WebDriver driver, String username, String password, Long loginWaitMillis) throws InterruptedException {
-            driver.navigate().to("https://en.onlinesoccermanager.com/PrivacyNotice?nextUrl=%2FLogin");
             WebElement button = driver.findElement(By.cssSelector("#page-privacynotice > div > div > div:nth-child(2) > div:nth-child(3) > div > button"));
             button.click();
 
@@ -222,39 +187,42 @@ public class Main {
             WebElement usernameField = driver.findElement(By.cssSelector("#manager-name"));
             WebElement passwordField = driver.findElement(By.cssSelector("#password"));
             WebElement actualLoginButton = driver.findElement(By.cssSelector("#login"));
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
+            Thread.currentThread().sleep(loginWaitMillis);
             usernameField.sendKeys(username);
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
+            Thread.currentThread().sleep(loginWaitMillis);
             passwordField.sendKeys(password);
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
+            Thread.currentThread().sleep(loginWaitMillis);
             new Actions(driver).moveToElement(actualLoginButton).build().perform();
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
             actualLoginButton.submit();
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis * 2));
-            if (driver.getCurrentUrl().equalsIgnoreCase("https://en.onlinesoccermanager.com/ChooseLeague")) {
+//            actualLoginButton.click();
+            WebDriverWait wdw = new WebDriverWait(driver, Duration.ofMillis(loginWaitMillis));
+            WebElement until = wdw.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#manager-name")));
+            if (until != null) {
                 System.out.println("Login işlemi başarılı");
                 return;
             }
-            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
+            Thread.currentThread().sleep(loginWaitMillis);
             List<WebElement> elements = driver.findElements(By.cssSelector("#page-signup > div.page.content.hidden-before-binding > div.register-information-container.horizontal-center-absolute > div.register-information-block.buttons > button"));
-            if (!elements.isEmpty() && elements.get(0).isDisplayed()) {
+            if (elements.size() > 0 && elements.get(0).isDisplayed()) {
                 elements.get(0).click();
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
+                Thread.currentThread().sleep(loginWaitMillis);
                 usernameField = driver.findElement(By.cssSelector("#manager-name"));
                 passwordField = driver.findElement(By.cssSelector("#password"));
                 actualLoginButton = driver.findElement(By.cssSelector("#login"));
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
+                Thread.currentThread().sleep(loginWaitMillis);
                 usernameField.sendKeys(username);
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
+                Thread.currentThread().sleep(loginWaitMillis);
                 passwordField.sendKeys(password);
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
+                Thread.currentThread().sleep(loginWaitMillis);
                 actualLoginButton.click();
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(loginWaitMillis));
-                if (driver.getCurrentUrl().equalsIgnoreCase("https://en.onlinesoccermanager.com/ChooseLeague")) {
+                Thread.currentThread().sleep(loginWaitMillis);
+                WebDriverWait wdw2 = new WebDriverWait(driver, Duration.ofMillis(loginWaitMillis));
+                WebElement until1 = wdw2.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#manager-name")));
+                if (until1.isDisplayed()) {
                     System.out.println("Login işlemi başarılı");
                     return;
-                } else {
+                }
+                else {
                     System.err.println("Login işlemi başarısız");
                     throw new RuntimeException();
                 }
